fae46e5a1656f09b46995c1622ad2da16562b6ee
Daniel alejandro Arevalo Guecha <127900056+Akarviii@users.noreply.github.com>
2026-02-26 14:04:13 -0500

    styles for the whole app

---
+++ src/styles/colors.js
@@ -0,0 +1,12 @@
+﻿export const Colors = {
+  primary: '#FFB7C5',
+  secondary: '#CF4B43',
+  background: '#ffffff',
+  surface: '#F2F2F2',
+  text: '#363F74',
+  textLight: '#0E0E0E',
+  success: '#2E7D32',
+  warning: '#F9A825',
+  error: '#D32F2F',
+  border: '#E3F2FD',
+};
